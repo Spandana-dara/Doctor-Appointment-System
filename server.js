6e28f5c74a676cf1bd0339056187da5435d683de
Spandana <spandana1286@gmail.com>
2024-02-24 00:11:51 -0500

    created server

--- server.js
+++ server.js
@@ -3,9 +3,28 @@ const morgan = require("morgan");
 const colors = require("colors");
 const dotenv = require("dotenv");
 
+// env config
+dotenv.config();
+
 //create rest object
 const app = express();
 
 //middleware
 app.use(morgan("dev"));
 app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+  res.status(200).send({
+    message: "server running",
+  });
+});
+
+const port = process.env.PORT || 8080;
+
+//listen
+app.listen(port, () => {
+  console.log(
+    `Server running in ${process.env.DEV_MODE} mode on port ${process.env.PORT}`
+  );
+});
