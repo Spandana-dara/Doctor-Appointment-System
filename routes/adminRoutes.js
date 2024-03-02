68861e9ab71a16868185542e20b137f92ddeed38
Spandana <spandana1286@gmail.com>
2024-03-01 22:50:48 -0500

    users and doctors list

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,8 +1,15 @@
 const express = require("express");
-
+const authMiddleware = require("../middlewares/authMiddleware");
+const {
+  getAllUsersController,
+  getAllDoctorsController,
+} = require("../controllers/adminController");
 const router = express.Router();
 
 // Get method || Users
-router.get();
+router.get("/getAllUsers", authMiddleware, getAllUsersController);
+
+//GET METHOD || DOCTORS
+router.get("/getAllDoctors", authMiddleware, getAllDoctorsController);
 
 module.exports = router;
