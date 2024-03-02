a7a88594799b237ada2f8e6f3adc43b55e39b508
Spandana <spandana1286@gmail.com>
2024-03-01 23:46:17 -0500

    approve doctor

--- models/doctorModel.js
+++ models/doctorModel.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 
 const doctorSchema = new mongoose.Schema(
   {
+    userId: {
+      type: String,
+    },
     firstName: {
       type: String,
       required: [true, "First name is required"],
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,6 +3,7 @@ const authMiddleware = require("../middlewares/authMiddleware");
 const {
   getAllUsersController,
   getAllDoctorsController,
+  changeAccountStatusController,
 } = require("../controllers/adminController");
 const router = express.Router();
 
@@ -11,5 +12,12 @@ router.get("/getAllUsers", authMiddleware, getAllUsersController);
 
 //GET METHOD || DOCTORS
 router.get("/getAllDoctors", authMiddleware, getAllDoctorsController);
+
+//POST ACCOUNT STATUS
+router.post(
+  "/changeAccountStatus",
+  authMiddleware,
+  changeAccountStatusController
+);
 
 module.exports = router;
